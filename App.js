c8a71d41793a2e255ba07a7a83a10aa5a52bd864
Chiem Trieu Phu <63228025+phubierd@users.noreply.github.com>
2023-09-10 22:05:22 +0700

    first commit tailwind css

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500 text-4xl">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
